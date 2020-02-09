a057dccb9b1ac5cc8318cf505f477c7248c348e5
WhoIsDoctorWho <31898055+WhoIsDoctorWho@users.noreply.github.com>
2020-02-09 16:34:26 +0200

    Test task done

---
+++ WorkDayCalculator.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace CSharpTest
+{
+    public class WorkDayCalculator : IWorkDayCalculator
+    {
+        public DateTime Calculate(DateTime startDate, int dayCount, WeekEnd[] weekEnds)
+        {
+            if (dayCount < 0)
+                throw new ArgumentException("Incorrect count of days");
+            DateTime expectedEndDate = startDate.AddDays(dayCount-1); // Start date is the first day of the period.
+            if (weekEnds == null || weekEnds.Length == 0)            
+                return expectedEndDate;           
+            foreach(WeekEnd weekEnd in weekEnds)
+            {
+                if(!startDate.IsTheSameKind(weekEnd.StartDate, weekEnd.EndDate))
+                    throw new ArgumentException("Input dates have different kind");
+                bool isIntersectWeekEnd = weekEnd.StartDate.IsInRange(startDate, expectedEndDate);
+                if (isIntersectWeekEnd)
+                {
+                    int weekEndDays = LengthOfTimeInterval(weekEnd.StartDate, weekEnd.EndDate);
+                    expectedEndDate = expectedEndDate.AddDays(weekEndDays);
+                }
+                else if (weekEnd.StartDate <= startDate && weekEnd.EndDate >= startDate)
+                {
+                    startDate = weekEnd.EndDate;
+                    expectedEndDate = startDate.AddDays(dayCount - 1);
+                }
+                else break;
+            }
+            return expectedEndDate;         
+        }        
+        private int LengthOfTimeInterval(DateTime startDate, DateTime endDate)
+        {
+            return endDate.Subtract(startDate).Days+1; // length of [t1;t2] interval
+        }
+    }
+    public static class DateTimeExtension
+    {
+        public static bool IsInRange(this DateTime dateToCheck, DateTime startDate, DateTime endDate)
+        {
+            return dateToCheck >= startDate && dateToCheck <= endDate;
+        }
+        public static bool IsTheSameKind(this DateTime dateToCheck, params DateTime[] dateTimes)
+        {
+            foreach(DateTime dt in dateTimes)
+            {
+                if (dateToCheck.Kind != dt.Kind)
+                    return false;
+            } 
+            return true;
+        }
+    }
+}
---
+++ WorkDayCalculatorTests.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace CSharpTest
+{
+    [TestClass]
+    public class WorkDayCalculatorTests
+    {
+
+        [TestMethod]
+        public void TestNoWeekEnd()
+        {
+            DateTime startDate = new DateTime(2014, 12, 1);
+            int count = 10;
+
+            DateTime result = new WorkDayCalculator().Calculate(startDate, count, null);
+
+            Assert.AreEqual(startDate.AddDays(count-1), result);
+        }
+
+        [TestMethod]
+        public void TestNormalPath()
+        {
+            DateTime startDate = new DateTime(2017, 4, 21);
+            int count = 5;
+            WeekEnd[] weekends = new WeekEnd[1]
+            {
+                new WeekEnd(new DateTime(2017, 4, 23), new DateTime(2017, 4, 25))
+            }; 
+
+            DateTime result = new WorkDayCalculator().Calculate(startDate, count, weekends);
+
+            Assert.IsTrue(result.Equals(new DateTime(2017, 4, 28)));
+        }
+
+        [TestMethod]
+        public void TestWeekendAfterEnd()
+        {
+            DateTime startDate = new DateTime(2017, 4, 21);
+            int count = 5;
+            WeekEnd[] weekends = new WeekEnd[2]
+            {
+                new WeekEnd(new DateTime(2017, 4, 23), new DateTime(2017, 4, 25)),
+                new WeekEnd(new DateTime(2017, 4, 29), new DateTime(2017, 4, 29))
+            };
+            
+            DateTime result = new WorkDayCalculator().Calculate(startDate, count, weekends);
+
+            Assert.IsTrue(result.Equals(new DateTime(2017, 4, 28)));
+        }        
+        [TestMethod]
+        public void TestSameStartEndDates()
+        {
+            DateTime startDate = new DateTime(2012, 2, 1);
+            int count = 10;
+            WeekEnd[] weekends = new WeekEnd[1]
+            {
+                new WeekEnd(new DateTime(2012, 2, 5), new DateTime(2012, 2, 5))
+            };
+
+            DateTime result = new WorkDayCalculator().Calculate(startDate, count, weekends);
+
+            Assert.IsTrue(result.Equals(new DateTime(2012, 2, 11)));
+        }
+        [TestMethod]
+        public void TestWeekEndStartBeforeAndEndAfterStartDate()
+        {
+            DateTime startDate = new DateTime(2015, 4, 21);
+            int count = 5;
+            WeekEnd[] weekends = new WeekEnd[1]
+            {
+                new WeekEnd(new DateTime(2015, 4, 19), new DateTime(2015, 4, 23))
+            };
+
+            DateTime result = new WorkDayCalculator().Calculate(startDate, count, weekends);
+
+            Assert.IsTrue(result.Equals(new DateTime(2015, 4, 27)));
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestNegativeDaysCount()
+        {            
+            DateTime result = new WorkDayCalculator().Calculate(DateTime.Now, -42, null);            
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestDifferentKindOfTimeZones()
+        {
+            DateTime startDate = DateTime.UtcNow;
+            int count = 5;
+            WeekEnd[] weekends = new WeekEnd[1]
+            {
+                new WeekEnd(DateTime.Now, DateTime.Now)
+            };
+
+            DateTime result = new WorkDayCalculator().Calculate(startDate, count, weekends); // should throw an exception                        
+        }
+    }
+}
